14f38bfccb5f92df944c06c18a6043e20f56dd90
deriva <602392714@qq.com>
2021-12-23 17:48:25 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace OfficeMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.labtime = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstMusicItem = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(325, 426);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(401, 167);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "开启";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // labtime
+            // 
+            this.labtime.AutoSize = true;
+            this.labtime.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labtime.Location = new System.Drawing.Point(399, 47);
+            this.labtime.Name = "labtime";
+            this.labtime.Size = new System.Drawing.Size(125, 46);
+            this.labtime.TabIndex = 2;
+            this.labtime.Text = "label1";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(515, 167);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "暂停";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "刷新";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstMusicItem
+            // 
+            this.lstMusicItem.FormattingEnabled = true;
+            this.lstMusicItem.ItemHeight = 12;
+            this.lstMusicItem.Location = new System.Drawing.Point(343, 237);
+            this.lstMusicItem.Name = "lstMusicItem";
+            this.lstMusicItem.Size = new System.Drawing.Size(445, 196);
+            this.lstMusicItem.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstMusicItem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.labtime);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "办公提醒";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label labtime;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstMusicItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OfficeMusic
+{
+    public partial class Form1 : Form
+    {
+        private static int OpenState = 0;
+        private static string OpenTime = "";//开启播放时间
+        private static string StopTime = "";//停止时间
+        private static Dictionary<string,string> dicMusic;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var st = DateTime.Now.ToString("HH-mm-ss");
+            var dt = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            if (OpenState == 1)
+            {
+                var timemusic = 0;
+                if (StopTime == st) { Stop(); }
+            }
+            else
+            {
+                var musiDanCc = dicMusic.Where(x => x.Key.StartsWith(st)).Count();
+                if (musiDanCc>0)
+                {
+                  var dd=  dicMusic.Where(x => x.Key.StartsWith(st)).First();
+                    var url = dd.Value;
+                    OpenTime = st;
+                    var sce = int.Parse(dd.Key.Split('-')[3]);//.ToInt();
+                    StopTime = DateTime.Now.AddSeconds(sce).ToString("HH-mm-ss");
+                    Play(url);
+                }
+
+            }
+
+            labtime.Text = dt;
+
+
+        }
+
+        private string GetFileInfo()
+        {
+            var fileinfo = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "/res/" + OpenTime);
+            if (fileinfo.Length > 0)
+                return fileinfo[0];
+            return "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labtime.Text = "等待";
+            InitMusic();
+          
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1000;  //定时器时间间隔
+            timer1.Start();   //定时器开始工作
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer1.Stop();
+        }
+
+        private void Play(string url)
+        {
+         //   var url = AppDomain.CurrentDomain.BaseDirectory + "/res/李丽芬 - 爱江山更爱美人.mp3";
+            axWindowsMediaPlayer1.URL = url;
+            OpenState = 1;
+            // axWindowsMediaPlayer1.play
+        }
+
+        private void Stop()
+        {
+            var url = AppDomain.CurrentDomain.BaseDirectory + "/res/李丽芬 - 爱江山更爱美人.mp3";
+            axWindowsMediaPlayer1.URL = "";
+            OpenState = 0;
+            // axWindowsMediaPlayer1.play
+        }
+
+        private void InitMusic()
+        {
+            dicMusic = new Dictionary<string, string>();
+            dicMusic.Clear();
+            lstMusicItem.Items.Clear();
+            var root = AppDomain.CurrentDomain.BaseDirectory + "/res/";
+            var fileinfo = Directory.GetFiles(root);
+            if (fileinfo.Length > 0)
+            {
+                foreach(var it in fileinfo)
+                {
+                    if (it.Contains(".mp3"))
+                    {
+                        var time = it.Replace(root, "");
+                        dicMusic.Add(time, it);
+                        lstMusicItem.Items.Add(time);
+                    }
+                }
+             
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            InitMusic();
+        }
+    }
+}
